fd998631b60cfbcb6a7f46c0fd4eeeb50cf73791
boan-anbo <gazagoal@gmail.com>
2020-12-19 20:14:50 +0100

    haha

--- https-nginx-setup/conf.d
+++ https-nginx-setup/conf.d
@@ -1,8 +1,8 @@
 server {
     listen              443 ssl;
     server_name         worldmiao.com www.worldmiao.com;
-    ssl_certificate     /etc/letsencrypt/live/server.company.com/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/server.company.com/privkey.pem;
+    ssl_certificate     /etc/letsencrypt/live/worldmiao.com/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/worldmiao.com/privkey.pem;
 
     location / {
         ...
